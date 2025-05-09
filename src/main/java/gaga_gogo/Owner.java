49d63e31a1af0669a27115d456007a2eddaa62cd
parkgaga0720 <parkgaga.0720@gmail.com>
2025-05-09 15:43:09 +0900

    코드 수정

--- src/main/java/gaga_gogo/Owner.java
+++ src/main/java/gaga_gogo/Owner.java
@@ -5,11 +5,13 @@ public class Owner {
     private final Dog gogo = new Dog();
 
     public void gogoSit() {
-        if (gogo.isStanding()){
+        if (gogo.isStanding()) {
             gogo.sitting();
+        }else if(gogo.isSitting() || gogo.isWaiting() || gogo.isEating()) {
+            System.out.println("고고는 이미 앉아 있습니다.");
+        }else{
+            System.out.println("산책 중에는 바닥이 더러워서 앉을 수 없습니다.");
         }
-        System.out.println("고고는 지금 앉을 수 없습니다.");
-        return;
     }
 
     public void gogoWait() {
